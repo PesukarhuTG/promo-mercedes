c3b56a10f229cd7d059995903096db4229ca8ed3
Tatiana <ganzhinovat@yandex.ru>
2021-09-21 20:05:07 +0300

    Update Accordeon: close option if it was opened earlier

--- js/accordeon.js
+++ js/accordeon.js
@@ -30,12 +30,17 @@ function accordeon() {
         const btn = target.closest('button'); //ищем родитель button
 
         if (target && btn) { //если по тому, что кликнули имеет родителя button
-            featureLinkButtons.forEach((item, i) => { //обращаемся к псевдомассиву наших кнопок-заголовков
-                if (btn === item) { //если элемент псевдомасива совпадает с эл-том, куда кликнул пользователь
-                    hideContent(); //скрываем все характеристики
-                    showContent(i); //отражаем только текущую
-                }
-            })
+
+            if (btn.classList.contains('feature__link_active')) { //если кликаем дважды, то скрываем контент
+                hideContent();
+            } else {
+                featureLinkButtons.forEach((item, i) => { //обращаемся к псевдомассиву наших кнопок-заголовков
+                    if (btn === item) { //если элемент псевдомасива совпадает с эл-том, куда кликнул пользователь
+                        hideContent(); //скрываем все характеристики
+                        showContent(i); //отражаем только текущую
+                    }
+                })
+            }
         }
     })
 };
