8730426271a070fe3cdeff393826fb23f42516e4
Tatiana <ganzhinovat@yandex.ru>
2021-09-27 14:59:51 +0300

    Fix bug with tabs

--- js/tabs.js
+++ js/tabs.js
@@ -2,24 +2,31 @@ function tabs() {
 
     const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]'); //все кнопки-заголовки по атрибуту
     const tabsContentElems = document.querySelectorAll('[data-tabs-field]'); //все описания-заголовков по атрибуту
-    const tabsTitles = document.querySelectorAll('.design__title');
+    const tabsHeadlingElems = document.querySelectorAll('[data-tabs-headling]'); //все заголовки h2 по атрибуту
 
     for (let btn of tabsHandlerElems) { //перебор циклом for...of по новому стандарту
         btn.addEventListener('click', () => {
 
-            tabsHandlerElems.forEach((elem, index) => {
+            tabsHandlerElems.forEach((elem) => {
 
                 elem.classList.remove('design-list__item_active');
                 btn.classList.add('design-list__item_active');
-                tabsTitles[index].classList.toggle('hidden');
 
                 tabsContentElems.forEach(content => {
-                    if (content.dataset.tabsField === btn.dataset.tabsHandler) { //если атрибут описания совпадает с атрибутом кнопки-заголовка
+                    if (content.dataset.tabsField === btn.dataset.tabsHandler) { //если атрибут описания совпадает с атрибутом кнопки
                         content.classList.remove('hidden');
                     } else {
                         content.classList.add('hidden');
                     }
                 });
+
+                tabsHeadlingElems.forEach(headling => {
+                    if (headling.dataset.tabsHeadling === btn.dataset.tabsHandler) { //если атрибут h2 совпадает с атрибутом кнопки
+                        headling.classList.remove('hidden');
+                    } else {
+                        headling.classList.add('hidden');
+                    }
+                })
             })
         })
     }
